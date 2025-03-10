4025a2bedd24e2f79bf37cf89c9fd06bf3a99047
Михаил Моисеев <mmoiseev@iitdgroup.ru>
2025-03-10 18:30:34 +0400

    transfer LoginRequest.java

--- Slam/src/main/java/com/system/slam/web/dto/LoginRequest.java
+++ Slam/src/main/java/com/system/slam/web/dto/LoginRequest.java
@@ -1,30 +1,31 @@
 package com.system.slam.web.dto;
 
 public class LoginRequest {
-    private String email;
+    private String username;
     private String password;
 
-    public LoginRequest() {}
+    public LoginRequest() {
+    }
 
-    public LoginRequest(String email,
+    public LoginRequest(String username,
                         String password) {
-        this.email = email;
+        this.username = username;
         this.password = password;
     }
 
-    public String getEmail() {
-        return email;
+    public String getUsername() {
+        return username;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword() {
+    public void setPassword(String password) {
         this.password = password;
     }
 }
